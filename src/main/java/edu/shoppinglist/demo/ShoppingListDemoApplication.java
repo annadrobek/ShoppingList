0288b37a1e2d800948a13b535d8bcd6db583d57e
Anna Drobek <annadrobek@gmail.com>
2023-03-12 15:05:40 +0100

    Spring repo init

---
+++ src/main/java/edu/shoppinglist/demo/ShoppingListDemoApplication.java
@@ -0,0 +1,13 @@
+package edu.shoppinglist.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingListDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingListDemoApplication.class, args);
+	}
+
+}
