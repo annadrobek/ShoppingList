36a100bdcf1393c523011c9059b6534d8b927a5e
Anna Drobek <annadrobek@gmail.com>
2023-03-13 21:18:03 +0100

    Dodanie strony głównej

---
+++ src/main/java/edu/shoppinglist/demo/controllers/HomeController.java
@@ -0,0 +1,23 @@
+package edu.shoppinglist.demo.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ui.Model;
+
+@Controller
+@RequestMapping(path="/")
+public class HomeController {
+    
+  @Value("${spring.application.name}")
+    String appName;
+
+    @GetMapping("/")
+    public String showHomePage(Model model) {
+        model.addAttribute("appName", appName);
+        return "home";
+    }
+}
+
+
---
+++ src/main/java/edu/shoppinglist/demo/repositories/ItemRepository.java
@@ -0,0 +1,8 @@
+package edu.shoppinglist.demo.repositories;
+
+import edu.shoppinglist.demo.merchandise.Item;
+import org.springframework.data.repository.CrudRepository;
+
+public interface ItemRepository extends CrudRepository<Item, Integer> {
+
+}
