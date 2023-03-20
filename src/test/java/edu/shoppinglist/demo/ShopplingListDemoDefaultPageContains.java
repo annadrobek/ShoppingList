117b3f88410a0f79b889e555367b03fa9f2f1568
Anna Drobek <annadrobek@gmail.com>
2023-03-20 22:36:01 +0100

    Test zawartości strony

---
+++ src/test/java/edu/shoppinglist/demo/ShopplingListDemoDefaultPageContains.java
@@ -0,0 +1,30 @@
+package edu.shoppinglist.demo;
+
+import org.junit.jupiter.api.Test;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.beans.factory.annotation.Value;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+public class ShopplingListDemoDefaultPageContains {
+
+	@Value(value="${local.server.port}")
+	private int port;
+        
+        @Value(value="${default.web.page.contains}")
+	private String contains;
+
+	@Autowired
+	private TestRestTemplate restTemplate;
+
+	@Test
+	public void greetingShouldReturnDefaultMessage() throws Exception {
+		assertThat(this.restTemplate.getForObject("http://localhost:" + port + "/",
+				String.class)).contains(contains);
+	}
+}
